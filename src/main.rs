4a7a064caee2d4e9306781a7636fa93542ee56fe
Neal Wang <nealwang.sh@protonmail.com>
2022-08-10 01:09:00 -0700

    better animation

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,8 @@ use ggez::{
 	timer, Context, ContextBuilder, GameError, GameResult,
 };
 
-const WINDOW_SIZE: (f32, f32) = (1200.0, 800.0);
-const BOARD_SIZE: (usize, usize, usize) = (16, 16, 4);
+const WINDOW_SIZE: (f32, f32) = (1680.0, 1050.0);
+const BOARD_SIZE: (usize, usize, usize) = (24, 24, 4);
 const TILE_SIZE: (f32, f32) = (64.0, 64.0);
 
 struct State {
@@ -46,11 +46,13 @@ impl EventHandler<GameError> for State {
 							x: (x as f32 - y as f32 - 1.0) * TILE_SIZE.0 / 2.0
 								+ WINDOW_SIZE.0 / 2.0,
 							y: (x as f32 * 0.5 + y as f32 * 0.5) * TILE_SIZE.1 / 2.0
-								+ WINDOW_SIZE.1 / 4.0 + ((timer::time_since_start(ctx)
-								.as_secs_f32() * 4.0 - (BOARD_SIZE.0
-								as f32 - 1.0 - x
-								as f32) * 0.5 - y as f32)
-								.sin() * 16.0)
+								+ WINDOW_SIZE.1 / 8.0 + (((timer::time_since_start(ctx)
+								.as_secs_f32() * 2.0 + x as f32 * 0.25)
+								.sin() + (timer::time_since_start(ctx)
+								.as_secs_f32() * 2.0 + y
+								as f32
+								* 0.25)
+								.cos()) * 32.0)
 								.round(),
 						}),
 				)?;
