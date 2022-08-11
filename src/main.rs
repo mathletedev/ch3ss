cde1bff6d48e214504a097bb3d85d69d5d0771cc
Neal Wang <nealwang.sh@protonmail.com>
2022-08-10 18:48:25 -0700

    basic chess board

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,32 @@
+use std::f32::consts::PI;
+
 use ggez::{
 	conf::WindowMode,
-	event::{self, EventHandler},
+	event::{self, EventHandler, KeyCode},
 	graphics::{self, Color, DrawParam, Image, Rect},
 	mint::Point2,
 	timer, Context, ContextBuilder, GameError, GameResult,
 };
 
-const WINDOW_SIZE: (f32, f32) = (1680.0, 1050.0);
-const BOARD_SIZE: (usize, usize, usize) = (24, 24, 4);
+const WINDOW_SIZE: (f32, f32) = (800.0, 800.0);
+const BOARD_SIZE: (usize, usize, usize) = (8, 8, 4);
 const TILE_SIZE: (f32, f32) = (64.0, 64.0);
+const ANIMATION_SPEED: f32 = 8.0;
+const ANIMATION_SCALE: f32 = 32.0;
+const HIDDEN_OPACITY: f32 = 0.1;
 
 struct State {
 	sprite_sheet: Image,
+	focused_layer: i8,
+	animation_init: f32,
 }
 
 impl State {
 	fn new(ctx: &mut Context) -> GameResult<State> {
 		Ok(State {
-			sprite_sheet: graphics::Image::new(ctx, "/spritesheet.png")?,
+			sprite_sheet: Image::new(ctx, "/spritesheet.png")?,
+			focused_layer: -1,
+			animation_init: f32::MAX,
 		})
 	}
 }
@@ -32,36 +41,88 @@ impl EventHandler<GameError> for State {
 
 		for x in 0..BOARD_SIZE.0 {
 			for y in 0..BOARD_SIZE.1 {
-				graphics::draw(
-					ctx,
-					&self.sprite_sheet,
-					DrawParam::default()
-						.src(Rect {
-							x: if (x + y) % 2 == 0 { 0.5 } else { 0.0 },
-							y: 0.0,
-							w: 0.5,
-							h: 1.0,
-						})
-						.dest(Point2 {
-							x: (x as f32 - y as f32 - 1.0) * TILE_SIZE.0 / 2.0
-								+ WINDOW_SIZE.0 / 2.0,
-							y: (x as f32 * 0.5 + y as f32 * 0.5) * TILE_SIZE.1 / 2.0
-								+ WINDOW_SIZE.1 / 8.0 + (((timer::time_since_start(ctx)
-								.as_secs_f32() * 2.0 + x as f32 * 0.25)
-								.sin() + (timer::time_since_start(ctx)
-								.as_secs_f32() * 2.0 + y
-								as f32
-								* 0.25)
-								.cos()) * 32.0)
-								.round(),
-						}),
-				)?;
+				for z in 0..BOARD_SIZE.2 {
+					let elapsed = timer::time_since_start(ctx).as_secs_f32() - self.animation_init;
+
+					graphics::draw(
+						ctx,
+						&self.sprite_sheet,
+						DrawParam::default()
+							.src(Rect {
+								x: if (x + y) % 2 == 0 { 0.5 } else { 0.0 },
+								y: 0.0,
+								w: 0.5,
+								h: 1.0,
+							})
+							.dest(Point2 {
+								x: (x as f32 - y as f32 - 1.0) * TILE_SIZE.0 / 2.0
+									+ WINDOW_SIZE.0 / 2.0,
+								y: (x as f32 * 0.5 + y as f32 * 0.5) * TILE_SIZE.1 / 2.0
+									+ 100.0 + z as f32 * TILE_SIZE.1 * 2.0
+									+ if elapsed > PI / 4.0
+										|| (self.focused_layer != -1
+											&& self.focused_layer != z as i8)
+									{
+										0.0
+									} else {
+										((elapsed * ANIMATION_SPEED + ((x + y) as f32) * PI / 14.0)
+											.sin() * ANIMATION_SCALE)
+											.min(0.0)
+									},
+							})
+							.color(
+								if self.focused_layer == -1 || self.focused_layer == z as i8 {
+									Color::WHITE
+								} else {
+									Color {
+										r: 1.0,
+										g: 1.0,
+										b: 1.0,
+										a: HIDDEN_OPACITY,
+									}
+								},
+							),
+					)?;
+				}
 			}
 		}
 
 		graphics::present(ctx)?;
 
 		Ok(())
+	}
+
+	fn key_down_event(
+		&mut self,
+		ctx: &mut Context,
+		keycode: event::KeyCode,
+		_keymods: event::KeyMods,
+		_repeat: bool,
+	) {
+		match keycode {
+			KeyCode::Space => self.animation_init = timer::time_since_start(ctx).as_secs_f32(),
+			KeyCode::Key0 => {
+				self.animation_init = timer::time_since_start(ctx).as_secs_f32();
+				self.focused_layer = -1;
+			}
+			KeyCode::Key1 => {
+				self.animation_init = timer::time_since_start(ctx).as_secs_f32();
+				self.focused_layer = 0;
+			}
+			KeyCode::Key2 => {
+				self.animation_init = timer::time_since_start(ctx).as_secs_f32();
+				self.focused_layer = 1;
+			}
+			KeyCode::Key3 => {
+				self.animation_init = timer::time_since_start(ctx).as_secs_f32();
+				self.focused_layer = 2;
+			}
+			KeyCode::Key4 => {
+				self.animation_init = timer::time_since_start(ctx).as_secs_f32();
+				self.focused_layer = 3;
+			}
+			_ => {}
+		}
 	}
 }
 
